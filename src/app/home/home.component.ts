e29edc4ace4672b52057c2061cc9e94bfe2227d4
Cedric Nishimwe <cedric.nishimwe.1@gmail.com>
2021-06-30 10:06:47 +0200

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import {Emitters} from '../emitters/emitters'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+
+
+  message: string = 'you are not logged in';
+
+
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:8000/api/user', {withCredentials: true}).subscribe(
+      (res: any) => {
+        this.message = `Hi ${res.name}`;
+        Emitters.authEmitter.emit(true);
+
+      },
+      err => {
+        this.message = 'you are not logged in';
+        Emitters.authEmitter.emit(false);
+      }
+    );
+  }
+
+}
